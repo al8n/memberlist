731cd10ea73079cbee7068d47edcc103bcb9c880
al8n <scygliu1@gmail.com>
2023-06-22 16:17:48 +0800

    WIP: offload CPU bound computation

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,5 @@
 #![forbid(unsafe_code)]
-// #![deny(warnings)]
+#![deny(warnings)]
 #![cfg_attr(feature = "nightly", feature(return_position_impl_trait_in_trait))]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(docsrs, allow(unused_attributes))]
