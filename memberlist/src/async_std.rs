a5754873d2ee243633671c71016046b7994067d8
Al Liu <scygliu1@gmail.com>
2024-03-05 02:09:25 +0800

    Add missing type aliases

--- memberlist/src/async_std.rs
+++ memberlist/src/async_std.rs
@@ -1,1 +1,83 @@
+use agnostic::async_std::AsyncStdRuntime;
 
+/// Memberlist type alias for using [`NetTransport`](memberlist_net::NetTransport) and [`Tcp`](memberlist_net::stream_layer::tcp::Tcp) stream layer with `async_std` runtime.
+#[cfg(all(
+  any(feature = "tcp", feature = "tls", feature = "native-tls"),
+  not(target_family = "wasm")
+))]
+#[cfg_attr(
+  docsrs,
+  doc(cfg(all(
+    any(feature = "tcp", feature = "tls", feature = "native-tls"),
+    not(target_family = "wasm")
+  )))
+)]
+pub type AsyncStdTcpMemberlist<I, A, W, D> = memberlist_core::Memberlist<
+  memberlist_net::NetTransport<
+    I,
+    A,
+    memberlist_net::stream_layer::tcp::Tcp<AsyncStdRuntime>,
+    W,
+    AsyncStdRuntime,
+  >,
+  D,
+>;
+
+/// Memberlist type alias for using [`NetTransport`](memberlist_net::NetTransport) and [`Tls`](memberlist_net::stream_layer::tls::Tls) stream layer with `async_std` runtime.
+#[cfg(all(feature = "tls", not(target_family = "wasm")))]
+#[cfg_attr(docsrs, doc(cfg(all(feature = "tls", not(target_family = "wasm")))))]
+pub type AsyncStdTlsMemberlist<I, A, W, D> = memberlist_core::Memberlist<
+  memberlist_net::NetTransport<
+    I,
+    A,
+    memberlist_net::stream_layer::tls::Tls<AsyncStdRuntime>,
+    W,
+    AsyncStdRuntime,
+  >,
+  D,
+>;
+
+/// Memberlist type alias for using [`NetTransport`](memberlist_net::NetTransport) and [`NativeTls`](memberlist_net::stream_layer::native_tls::NativeTls) stream layer with `async_std` runtime.
+#[cfg(all(feature = "native-tls", not(target_family = "wasm")))]
+#[cfg_attr(
+  docsrs,
+  doc(cfg(all(feature = "native-tls", not(target_family = "wasm"))))
+)]
+pub type AsyncStdNativeTlsMemberlist<I, A, W, D> = memberlist_core::Memberlist<
+  memberlist_net::NetTransport<
+    I,
+    A,
+    memberlist_net::stream_layer::native_tls::NativeTls<AsyncStdRuntime>,
+    W,
+    AsyncStdRuntime,
+  >,
+  D,
+>;
+
+/// Memberlist type alias for using [`QuicTransport`](memberlist_quic::QuicTransport) and [`Quinn`](memberlist_quic::stream_layer::quinn::Quinn) stream layer with `async_std` runtime.
+#[cfg(all(feature = "quinn", not(target_family = "wasm")))]
+#[cfg_attr(docsrs, doc(cfg(all(feature = "quinn", not(target_family = "wasm")))))]
+pub type AsyncStdQuicMemberlist<I, A, W, D> = memberlist_core::Memberlist<
+  memberlist_quic::QuicTransport<
+    I,
+    A,
+    memberlist_quic::stream_layer::quinn::Quinn<AsyncStdRuntime>,
+    W,
+    AsyncStdRuntime,
+  >,
+  D,
+>;
+
+/// Memberlist type alias for using [`QuicTransport`](memberlist_quic::QuicTransport) and [`S2n`](memberlist_quic::stream_layer::s2n::S2n) stream layer with `async_std` runtime.
+#[cfg(all(feature = "s2n", not(target_family = "wasm")))]
+#[cfg_attr(docsrs, doc(cfg(all(feature = "s2n", not(target_family = "wasm")))))]
+pub type AsyncStdS2nMemberlist<I, A, W, D> = memberlist_core::Memberlist<
+  memberlist_quic::QuicTransport<
+    I,
+    A,
+    memberlist_quic::stream_layer::s2n::S2n<AsyncStdRuntime>,
+    W,
+    AsyncStdRuntime,
+  >,
+  D,
+>;
