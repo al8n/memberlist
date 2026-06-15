f914aa46cd00098c1d6cd3ada029756a9a507972
al8n <scygliu1@gmail.com>
2026-06-15 14:04:25 +1200

    docs(memberlist): annotate feature-gated driver modules with doc(cfg)
    The tokio/smol/compio/smoltcp/embassy/reactor/embedded public modules now carry
    #[cfg_attr(docsrs, doc(cfg(feature = ...)))] so docs.rs shows their feature gate.

--- memberlist/src/lib.rs
+++ memberlist/src/lib.rs
@@ -16,22 +16,29 @@ extern crate alloc;
 pub use memberlist_proto as proto;
 
 #[cfg(feature = "tokio")]
+#[cfg_attr(docsrs, doc(cfg(feature = "tokio")))]
 pub mod tokio;
 
 #[cfg(feature = "smol")]
+#[cfg_attr(docsrs, doc(cfg(feature = "smol")))]
 pub mod smol;
 
 #[cfg(feature = "compio")]
+#[cfg_attr(docsrs, doc(cfg(feature = "compio")))]
 pub mod compio;
 
 #[cfg(feature = "smoltcp")]
+#[cfg_attr(docsrs, doc(cfg(feature = "smoltcp")))]
 pub mod smoltcp;
 
 #[cfg(feature = "embassy")]
+#[cfg_attr(docsrs, doc(cfg(feature = "embassy")))]
 pub mod embassy;
 
 #[cfg(feature = "reactor")]
+#[cfg_attr(docsrs, doc(cfg(feature = "reactor")))]
 pub mod reactor;
 
 #[cfg(feature = "embedded")]
+#[cfg_attr(docsrs, doc(cfg(feature = "embedded")))]
 pub mod embedded;
