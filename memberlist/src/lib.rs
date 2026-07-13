936327369fcfc2b3c85d759ed0ac0d3b9e0e5c7b
al8n <scygliu1@gmail.com>
2026-07-13 16:34:15 +1200

    docs: set the crate logo on every crate
    `#![doc(html_logo_url = ...)]` so rustdoc shows the memberlist mark instead of
    the default placeholder.

--- memberlist/src/lib.rs
+++ memberlist/src/lib.rs
@@ -1,4 +1,5 @@
 #![doc = include_str!("../README.md")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/al8n/memberlist/main/art/logo_72x72.png")]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
